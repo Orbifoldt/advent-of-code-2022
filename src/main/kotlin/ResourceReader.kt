dc628993154e7107b5907e8e4cc680fffc93d1ac
Orbifoldt <eikenrode02@gmail.com>
2022-12-10 23:40:16 +0100

    Adding more resource readers

--- src/main/kotlin/ResourceReader.kt
+++ src/main/kotlin/ResourceReader.kt
@@ -1,7 +1,24 @@
+import java.io.BufferedReader
 import java.io.FileNotFoundException
+import java.io.IOException
+import java.util.stream.Stream
 
 object ResourceReader {
-    fun readLines(filename: String) =
+
+    fun readBuffered(filename: String): BufferedReader =
         (javaClass.getResourceAsStream(filename) ?: throw FileNotFoundException(filename))
-        .bufferedReader().lines()
+            .bufferedReader()
+
+    fun readString(filename: String): String = readBuffered(filename).use { it.readText() }
+
+    fun readLines(filename: String): Stream<String> = readBuffered(filename).let { bufferedReader ->
+        bufferedReader.lines()
+            .onClose {
+                try {
+                    bufferedReader.close()
+                } catch (e: IOException) {
+                    throw RuntimeException(e)
+                }
+            }
+    }
 }
