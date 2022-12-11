e80123da7e18f877058a88ccb266161d90045045
Orbifoldt <eikenrode02@gmail.com>
2022-12-11 21:03:45 +0100

    Compactification!

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -10,10 +10,24 @@ fun main() {
     println("--- Part Two ---")
     println("The score according to the strategy is: ${part2("example-rps.txt")}")
     println("The score according to the strategy is: ${part2("my-rps.txt")}")
+
+    println("--- ALT ---")
+    alt("example-rps.txt")
+    alt("my-rps.txt")
 }
 
+// Compact (but also hardcoded) solution:
+fun String.moveScore() = if(this[2]=='X') 1 else if(this[2]=='Y') 2 else 3
+fun String.outcomeScore() = if(matches(Regex("A X|B Y|C Z"))) 3 else if (matches(Regex("A Y|B Z|C X"))) 6 else  0
+fun String.outcomeScorePart2() = if(this[2]=='X') 0 else if(this[2]=='Y') 3 else 6
+fun String.moveScorePart2() = if(matches(Regex("A Y|B X|C Z"))) 1 else if (matches(Regex("A Z|B Y|C X"))) 2 else  3
+fun alt(filename: String) = ResourceReader.readLines("day02/$filename").map { it.trim() }
+    .map { (it.moveScore() + it.outcomeScore()) to (it.outcomeScorePart2() + it.moveScorePart2()) }
+    .reduce { (p1ScoreA, p2ScoreA), (p1ScoreB, p2ScoreB) -> (p1ScoreA + p1ScoreB) to (p2ScoreA + p2ScoreB)}.get()
+    .also { (part1, part2) -> println("$filename: Score for part 1 is '$part1' and part 2 is '$part2'.") }
+
 fun part1(filename: String): Int =
-    ResourceReader.readColumns(filename).map { (othersMove, myMove, _) ->
+    ResourceReader.readColumns("day02/$filename").map { (othersMove, myMove, _) ->
         score(myMove.mapXyzToMove(), othersMove.mapAbcToMove())
     }.reduce(Int::plus).get()
 
@@ -46,7 +60,7 @@ val outcomeMatrix = listOf( /* Outcome is from perspective of the row */
 fun determineOutcome(myMove: Move, otherMove: Move) = outcomeMatrix[myMove.ordinal][otherMove.ordinal]
 
 fun part2(filename: String): Int =
-    ResourceReader.readColumns(filename).map { (othersMove, desiredOutcome, _) ->
+    ResourceReader.readColumns("day02/$filename").map { (othersMove, desiredOutcome, _) ->
         val other = othersMove.mapAbcToMove()
         val mine = determineMyMove(desiredOutcome.mapXyzToOutcome(), other)
         score(mine, other)
